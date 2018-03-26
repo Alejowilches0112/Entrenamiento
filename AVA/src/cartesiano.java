d3b17d12b152f628296bfaef19ae5e335b8f516b
Alejowilches0112 <cawilchesfe@poligran.edu.co>
2018-03-26 00:50:08 -0500

    Programas

---
+++ AVA/src/cartesiano.java
@@ -0,0 +1,13 @@
+
+public class cartesiano {
+	public static void main(String[] args) {
+		
+	}
+	static long productoSinRepetidos(int a[], int[]b) {
+		long x=a.length*b.length;
+		return x;
+	}
+	static void productoConRepetido() {
+		
+	}
+}
